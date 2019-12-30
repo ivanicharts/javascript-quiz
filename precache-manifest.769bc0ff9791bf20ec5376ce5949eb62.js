235d0d3a95e2fb9b44dd2b69797fd450beeff7b0
Ivan <ivan.icharts@gmail.com>
2019-12-30 21:36:42 +0200

    Updates

---
+++ precache-manifest.769bc0ff9791bf20ec5376ce5949eb62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da25005c340b99d79cf567420d957988",
+    "url": "/javascript-quizz/index.html"
+  },
+  {
+    "revision": "058eb89b83f0889a561b",
+    "url": "/javascript-quizz/static/css/2.72d72b5e.chunk.css"
+  },
+  {
+    "revision": "3114cfdf3fb12c4c247d",
+    "url": "/javascript-quizz/static/css/main.c5e1bf56.chunk.css"
+  },
+  {
+    "revision": "058eb89b83f0889a561b",
+    "url": "/javascript-quizz/static/js/2.c86bd6e4.chunk.js"
+  },
+  {
+    "revision": "3114cfdf3fb12c4c247d",
+    "url": "/javascript-quizz/static/js/main.089027e8.chunk.js"
+  },
+  {
+    "revision": "f9d11d42c46175d0b57f",
+    "url": "/javascript-quizz/static/js/runtime~main.6aa64b80.js"
+  }
+]);
