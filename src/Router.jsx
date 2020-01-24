8795c7b3feb00feb48809e6d6cf9d2b5476a23ec
Ivan Botnari <ivan.botnari@codefactorygroup.com>
2020-01-24 17:46:15 +0200

    Add linter and prettier

--- src/Router.jsx
+++ src/Router.jsx
@@ -3,7 +3,7 @@ import { Home } from './pages';
 import Questions from './pages/Questions';
 import { Progress } from './pages/Progress';
 import { Settings } from './pages/Settings';
-import { HashRouter, Switch, Route } from "react-router-dom";
+import { HashRouter, Switch, Route } from 'react-router-dom';
 
 export default function Router() {
   return (
@@ -15,5 +15,5 @@ export default function Router() {
         <Route path="/settings" component={Settings} />
       </Switch>
     </HashRouter>
-  )
+  );
 }
